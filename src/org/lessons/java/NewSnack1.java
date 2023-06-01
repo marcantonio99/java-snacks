c51afe6e680224e76ba2fa5ab7102235b5a402da
marcantonio99 <marcantonio8799@gmail.com>
2023-06-01 15:10:47 +0200

    NewSnack1 and NewSnack2

--- src/org/lessons/java/NewSnack1.java
+++ src/org/lessons/java/NewSnack1.java
@@ -6,7 +6,7 @@ public class NewSnack1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Inserisci un numero: ");
+        System.out.println("Inserisci un numero e lo stamperemo se pari!: ");
         int n = scan.nextInt();
 
         if (n % 2 != 0){
